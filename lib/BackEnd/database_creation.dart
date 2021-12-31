51ffc0fd8c2a3e95c6c8fc67e5b4117e47370a21
AhmedBouslama <46847587+AhmedBouslama@users.noreply.github.com>
2021-12-31 15:58:29 +0100

    update

--- lib/BackEnd/database_creation.dart
+++ lib/BackEnd/database_creation.dart
@@ -158,7 +158,7 @@ class Dbcreate {
       return Composant(
         id: maps[i]['id'],
         name: maps[i]['name'],
-        obtenue: DateTime.parse(maps[i]['obtenue'])∑,
+        obtenue: DateTime.parse(maps[i]['obtenue']),
         stock: maps[i]['stock'],
         category: maps[i]['FK_category'],
       );
