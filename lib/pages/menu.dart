b5570800a62fcf951ca722afd608f55ac068aa9f
moez ben hassen <moez.uzumaki@gmail.com>
2022-01-05 01:30:54 +0100

    ui changes x

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -11,10 +11,10 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        backgroundColor:  Color(0xFF2C2F33),
         appBar: AppBar(
           backgroundColor: Color(0xFFFF7643),
-          //foregroundColor: Color(0xFFFF7643),
+          foregroundColor: Color(0xFF2C2F33),
           title: const Text('Home Menu'),
         ),
         body: Center(
